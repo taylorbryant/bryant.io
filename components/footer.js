22fa6426f306d4694a48e3e7e981e166665b7a5d
Taylor Bryant <taylor@bryant.io>
2019-08-14 11:07:41 -0500

    Add Font Awesome icons

--- components/footer.js
+++ components/footer.js
@@ -1,27 +1,45 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedinIn,
+  faTwitter
+} from "@fortawesome/free-brands-svg-icons";
+
 import Container from "./container";
 
 function Footer() {
   return (
     <footer className="bg-gradient-green-blue">
       <Container>
-        <ul className="flex items-center justify-between text-sm text-white">
-          <li>
+        <nav className="flex items-center justify-between text-sm text-white">
+          <p>
             Created by{" "}
-            <a href="https://github.com/taylorbryant" target="_blank" className="font-bold">
-              Taylor Bryant
-            </a>
-          </li>
-
-          <li>
             <a
-              href="https://github.com/taylorbryant/bryant.io"
+              href="mailto:taylor@bryant.io"
               target="_blank"
               className="font-bold"
             >
-              GitHub
+              Taylor Bryant
             </a>
-          </li>
-        </ul>
+          </p>
+
+          <ul className="flex">
+            {[
+              { url: `https://github.com/taylorbryant`, icon: faGithub },
+              { url: `https://twitter.com/tayl_rbryant`, icon: faTwitter },
+              {
+                url: `https://www.linkedin.com/in/taylorjamesbryant/`,
+                icon: faLinkedinIn
+              }
+            ].map(item => (
+              <li className="ml-8" key={item.url}>
+                <a href={item.url} target="_blank">
+                  <FontAwesomeIcon className="h-8 w-8" icon={item.icon} />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </Container>
     </footer>
   );
