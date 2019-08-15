75d227e7e42b70babdd38a5aceef74c947d1e5a1
Taylor Bryant <taylor@bryant.io>
2019-08-15 00:29:20 -0500

    Update posts.js

--- data/posts.js
+++ data/posts.js
@@ -1,5 +1,5 @@
-/*eslint-env node*/
-const preval = require(`babel-plugin-preval/macro`);
+/* eslint-env node */
+import preval from "babel-plugin-preval/macro";
 
 const posts = preval`
   module.exports = require('./get-posts.js');
