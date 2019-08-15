91d995df0f0a5658611010b0cffbbe85192f7ee6
Taylor Bryant <taylor@bryant.io>
2019-08-14 20:33:26 -0500

    Replace copyright bullshit with a link to Next/Tailwind and the source code

--- components/footer.js
+++ components/footer.js
@@ -13,14 +13,23 @@ function Footer() {
       <Container>
         <nav className="flex items-center justify-between text-sm text-white">
           <p>
-            Created by{" "}
+            Built using{" "}
+            <a href="https://nextjs.org" target="_blank" className="font-bold">
+              Next.js
+            </a>{" "}
+            and{" "}
+            <a href="" target="_blank" className="font-bold">
+              Tailwind CSS
+            </a>
+            . Read the code{" "}
             <a
-              href="mailto:taylor@bryant.io"
+              href="https://github.com/taylorbryant/bryant.io"
               target="_blank"
               className="font-bold"
             >
-              Taylor Bryant
+              here
             </a>
+            .
           </p>
 
           <ul className="flex">
