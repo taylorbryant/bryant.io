eb74bbdf06fd8dfbc105ee6fc2e621483163b004
Taylor Bryant <taylor@bryant.io>
2019-08-19 08:25:17 -0500

    Change text to gray

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ import "../css/index.css";
 
 function Layout(props) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="text-gray-900 flex flex-col min-h-screen">
       <Header />
       <main className="flex-1">{props.children}</main>
       <Footer />
