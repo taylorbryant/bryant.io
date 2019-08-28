1e5be69a90957cbb6b78c6701ce64c3c5689757c
Taylor Bryant <taylor@bryant.io>
2019-08-28 11:45:33 -0500

    Try using non-promise API

--- move-public-files-to-root.js
+++ move-public-files-to-root.js
@@ -1,13 +1,7 @@
 /* eslint-env node */
 const fs = require(`fs`);
-const fsPromises = fs.promises;
 
-fsPromises
-  .copyFile(`./public/_redirects`, `./out/_redirects`)
-  .then(() => console.log(`_redirects was copied to ./out`))
-  .catch(() => console.error(`Error: _redirects could not be moved to ./out`));
-
-fsPromises
-  .copyFile(`./public/sitemap.xml`, `./out/sitemap.xml`)
-  .then(() => console.log(`sitemap.xml was copied to ./out`))
-  .catch(() => console.error(`Error: sitemap.xml could not be moved to ./out`));
+fs.copyFile(`./public/_redirects`, `./out/_redirects`, err => {
+  if (err) throw err;
+  console.log(`_redirects was copied to ./out/_redirects`);
+});
