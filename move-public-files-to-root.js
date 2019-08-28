820b7c5b97b76a0e3ddb20021c1482f35f2b3566
Taylor Bryant <taylor@bryant.io>
2019-08-28 11:43:02 -0500

    Try other thing

--- move-public-files-to-root.js
+++ move-public-files-to-root.js
@@ -1,10 +1,13 @@
 /* eslint-env node */
-const fs = require(`fs`).promises;
+const fs = require(`fs`);
+const fsPromises = fs.promises;
 
-fs.copyFile(`./public/_redirects`, `./out/_redirects`)
+fsPromises
+  .copyFile(`./public/_redirects`, `./out/_redirects`)
   .then(() => console.log(`_redirects was copied to ./out`))
   .catch(() => console.error(`Error: _redirects could not be moved to ./out`));
 
-fs.copyFile(`./public/sitemap.xml`, `./out/sitemap.xml`)
+fsPromises
+  .copyFile(`./public/sitemap.xml`, `./out/sitemap.xml`)
   .then(() => console.log(`sitemap.xml was copied to ./out`))
   .catch(() => console.error(`Error: sitemap.xml could not be moved to ./out`));
