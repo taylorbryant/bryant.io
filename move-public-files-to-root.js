856d4e7dcb28ce0bbc6587fd97d2d75c43bccc07
Taylor Bryant <taylor@bryant.io>
2019-08-28 11:50:19 -0500

    Add .nvmrc

--- move-public-files-to-root.js
+++ move-public-files-to-root.js
@@ -3,5 +3,10 @@ const fs = require(`fs`);
 
 fs.copyFile(`./public/_redirects`, `./out/_redirects`, err => {
   if (err) throw err;
-  console.log(`_redirects was copied to ./out/_redirects`);
+  console.log(`_redirects was copied to ./out`);
+});
+
+fs.copyFile(`./public/sitemap.xml`, `./out/sitemap.xml`, err => {
+  if (err) throw err;
+  console.log(`sitemap.xml was copied to ./out`);
 });
