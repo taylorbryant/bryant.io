2d2666a29d64c32e4d83ba4560307b12a967a474
Taylor Bryant <taylor@bryant.io>
2020-02-02 11:45:03 -0600

    Wrap index page in a section tag

--- src/components/index-page-layout.js
+++ src/components/index-page-layout.js
@@ -6,13 +6,13 @@ import PageLayout from "./page-layout";
 function IndexPageLayout({ children, title }) {
   return (
     <PageLayout>
-      <div className="flex flex-col md:justify-between md:flex-row">
+      <section className="flex flex-col md:justify-between md:flex-row">
         <div className="md:w-1/3">
           <h1 className="mb-6 text-3xl font-bold md:mb-0">{title}</h1>
         </div>
 
         <div className="md:w-2/3">{children}</div>
-      </div>
+      </section>
     </PageLayout>
   );
 }
