641c3563a4ad6ffa14ad011c1f00701215e094f5
Taylor Bryant <taylor@bryant.io>
2020-03-12 17:36:43 -0500

    Use Dracula code syntax theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,6 @@
 module.exports = {
   theme: {
     extend: {
-      colors: {
-        primary: `hsl(216, 50%, 80%)`,
-        secondary: `hsl(158, 50%, 80%)`,
-        light: `hsla(216, 49%, 80%, 0.3)`,
-        code: {
-          green: `hsl(158, 50%, 80%)`,
-          yellow: `hsl(61, 50%, 80%)`,
-          purple: `hsl(266, 50%, 80%)`,
-          red: `hsl(0, 50%, 80%)`,
-          blue: `hsl(208, 50%, 80%)`,
-          white: `#fff`
-        }
-      },
       margin: {
         9: `2.25rem`
       },
