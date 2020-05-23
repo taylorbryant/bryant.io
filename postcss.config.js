26fb0e2a124de748f586dcc3f57ea53eb109ec34
Taylor Bryant <taylor@bryant.io>
2020-05-22 21:33:03 -0500

    Move code out of /src and use Tailwind's built-in purge option

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,7 @@
 /* eslint-env node */
 module.exports = {
-  plugins: {
-    "postcss-import": {},
-    tailwindcss: {},
-    ...(process.env.NODE_ENV === `production`
-      ? {
-          "@fullhuman/postcss-purgecss": {
-            content: [`./src/**/*.js`],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-          autoprefixer: {},
-        }
-      : {}),
-  },
+  plugins: [
+    `tailwindcss`,
+    ...(process.env.NODE_ENV === `production` ? [`autoprefixer`] : []),
+  ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 const defaultTheme = require(`tailwindcss/defaultTheme`);
 
 module.exports = {
+  purge: [`./components/**/*.js`, `./pages/**/*.js`],
   theme: {
     extend: {
       fontFamily: {
@@ -14,7 +15,7 @@ module.exports = {
         9: `2.25rem`,
       },
     },
-    variants: {},
-    plugins: [],
   },
+  variants: {},
+  plugins: [],
 };
