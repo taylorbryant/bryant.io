e6d35169664b6f1ee0a54bb1eda3bb7a84e85cd1
Taylor Bryant <taylor@bryant.io>
2020-07-14 20:03:27 -0500

    Update page-layout.js

--- components/page-layout.js
+++ components/page-layout.js
@@ -5,7 +5,7 @@ import React from "react";
 function PageLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen space-y-12 text-gray-800 bg-gray-100 md:space-y-24">
-      <header className="w-full max-w-2xl px-3 pt-6 mx-auto text-white md:px-6">
+      <header className="w-full max-w-2xl px-3 pt-3 md:pt-6 mx-auto text-white md:px-6">
         <h1>
           <Link href="/">
             <a className="px-3 py-1 text-2xl no-underline bg-indigo-500">
@@ -17,7 +17,7 @@ function PageLayout({ children }) {
 
       <main className="flex-1 space-y-12 md:space-y-24">{children}</main>
 
-      <footer className="w-full max-w-2xl px-3 pb-6 mx-auto text-xs md:px-6">
+      <footer className="w-full max-w-2xl text-center md:text-left px-3 pb-3 md:pb-6 mx-auto text-xs md:px-6">
         Built using{` `}
         <a href="https://nextjs.org" rel="noopener noreferrer" target="_blank">
           Next.js
