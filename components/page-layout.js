bf059e42dbe44f57caa1696dfdb4d556892a42e6
Taylor Bryant <taylor@bryant.io>
2020-09-03 22:27:20 -0500

    Update page-layout.js

--- components/page-layout.js
+++ components/page-layout.js
@@ -5,10 +5,11 @@ import React from "react";
 function PageLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen space-y-12 text-gray-800 bg-gray-100 md:space-y-24">
-      <header className="w-full max-w-2xl px-3 pt-3 md:pt-6 mx-auto text-white md:px-6">
-        <h1>
+      <header className="w-full max-w-2xl px-3 pt-3 md:pt-6 mx-auto md:px-6 flex">
+        <img src="https://www.gravatar.com/avatar/889987f164dfd9c869bb863c40d9cc12" className="rounded-full w-8 h-8" />
+<h1>
           <Link href="/">
-            <a className="px-3 py-1 text-2xl no-underline bg-indigo-500">
+            <a className="ml-3 text-2xl no-underline">
               Taylor Bryant
             </a>
           </Link>
